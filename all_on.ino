e9d229727b07d7391b38ef5bebd570acca735575
bdpedigo <benjamindpedigo@gmail.com>
2018-01-15 16:48:53 -0800

    original
    number of lights on gradually increases, holds steady for 5 secs, then decreases

---
+++ all_on.ino
@@ -0,0 +1,35 @@
+void setup() {
+  // put your setup code here, to run once:
+  for (int i = 1; i <= 45; i++) {
+    pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);
+  }
+  for (int i = 1; i <= 45; i++) {
+    delay(100);
+    digitalWrite(i, HIGH);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for (int i = 1; i <= 45; i++) {
+    delay(100);
+    digitalWrite(i, HIGH);
+  } 
+  delay(5000);
+  for (int i = 45; i >= 1; i--) {
+    delay(100);
+    digitalWrite(i, LOW);
+  } 
+  
+  /*
+  for (int i = 1; i <= 45; i++) {
+    if (random(0,9) > 4){
+      digitalWrite(i, HIGH);
+    } else {
+      digitalWrite(i, LOW);
+    }
+  }
+  */
+  delay(100);
+}
